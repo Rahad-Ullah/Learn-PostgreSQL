f00b4d2165ba49e387ec8a3b4f36fcae1e6a2c87
Rahad-Ullah <myemail.abc20@gmail.com>
2024-11-02 20:41:24 +0600

    Learnt Group By in PostgreSQL

---
+++ groupBy.sql
@@ -0,0 +1,20 @@
+SELECT * FROM students
+
+
+-- group by country and select count, average age
+SELECT country, count(*), avg(age) FROM students
+    GROUP BY country
+
+
+-- group by country having average age getter than 20
+SELECT country, avg(age) FROM students
+    GROUP BY country
+        HAVING avg(age) > 20
+
+
+-- select students born in each year
+SELECT extract(YEAR FROM dob) as birth_year, count(*)
+    FROM students
+    GROUP BY birth_year
+
+
